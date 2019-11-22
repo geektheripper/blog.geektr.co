3e855aa0899faff1e6509cf66a833fdcdf9ec999
GeekTR <geektheripper@gmail.com>
2019-11-22 18:12:37 +0800

    add favicon

---
+++ .vuepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    title: 'GeekTR',
+    description: '@GeekTR //志在互联网，所好者繁，染拖延之疾，久治未愈，有乔翁之心，奈何命定庸人。',
+    head: [
+        ['link', { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+        ['link', { rel: 'icon', type: 'image/png', href: '/favicon.png' }]
+    ]
+}
